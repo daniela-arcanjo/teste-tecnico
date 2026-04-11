bdd253d6a7f79bc11d58ecfd2aee0d7219713e36
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2026-04-11 16:25:56 -0300

    refactor: standardize naming conventions across test files

---
+++ automation-web/cypress/e2e/cart.cy.js
@@ -0,0 +1,20 @@
+describe('Carrinho', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.loginWithValidCredentials()
+        cy.fixture('products').as('products')
+    })
+
+    it('Deve adicionar um produto ao carrinho com sucesso', function () {
+        cy.addProductToCart()
+        cy.contains('span', 'Your Cart').should('be.visible')
+        cy.contains('span', '1').should('be.visible')
+        cy.contains('div', this.products.sauceLabsBackpack.name).should('be.visible')
+    })
+
+    it('Deve remover um produto do carrinho com sucesso', function () {
+        cy.addProductToCart()
+        cy.removeProductToCart()
+        cy.contains('div', this.products.sauceLabsBackpack.name).should('not.exist')
+    })
+})
